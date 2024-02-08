bbba7e8ca6da86da9b2aa7c0e517ec2e6dc60ecd
bestzaza789 <146432574+bestzaza789@users.noreply.github.com>
2024-02-08 22:18:25 +0700

    feat:solve problem

--- ex-1.js
+++ ex-1.js
@@ -1,1 +1,17 @@
-function pivotIndex(nums) {}
+function pivotIndex(nums) {
+  let totalSum = nums.reduce((acc, curr) => acc + curr, 0);
+  let leftSum = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (leftSum === totalSum - leftSum - nums[i]) {
+      return i;
+    }
+    leftSum += nums[i];
+  }
+
+  return -1;
+}
+
+// Test the function
+let nums = [1, 7, 3, 6, 5, 6];
+console.log("Output:", pivotIndex(nums));
--- ex-2.js
+++ ex-2.js
@@ -1,1 +1,7 @@
-function defangIPaddr(nums) {}
+function defangIPaddr(nums) {
+  return address.replace(/\./g, "[.]");
+}
+
+// Test the function
+let address = "1.1.1.1";
+console.log("Output:", defangIPaddr(address));
